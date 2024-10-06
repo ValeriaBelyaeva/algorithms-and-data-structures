c21fc90837d21e703fb7c895eed60011f678c0a0
ValeriaBelyaeva <lera.belyayeva.2006@mail.ru>
2024-10-06 12:41:31 +0700

    Solve task 1insertion and tests

--- lab1/task1/src/insertion_sort.py
+++ lab1/task1/src/insertion_sort.py
@@ -1,2 +1,10 @@
-def merge_sort():
-    pass
+def insertion_sort(to_sort: list)-> list:
+    for i in range(1, len(to_sort)):
+        key = to_sort[i]
+        j = i - 1
+        while j >= 0 and key < to_sort[j]:
+            to_sort[j + 1] = to_sort[j]
+            j -= 1
+        to_sort[j + 1] = key
+
+    return to_sort
