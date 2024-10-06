9dfe1ed9fa5f96e1071128e4c6a5cc789bed5865
ValeriaBelyaeva <lera.belyayeva.2006@mail.ru>
2024-10-06 14:01:03 +0700

    added work with files

--- lab1/task1/src/insertion_sort_descending_order.py
+++ lab1/task1/src/insertion_sort_descending_order.py
@@ -8,3 +8,18 @@ def insertion_sort_reverse(to_sort):
         to_sort[j + 1] = key
 
     return to_sort
+
+inp_file = open("input")
+inp = list(map(int, inp_file.readline().split()))
+inp_file.close()
+out_file = open("output", 'w')
+try:
+    ans = insertion_sort_reverse(inp)
+    out = ''
+    for e in ans:
+        out = out + str(e) + ' '
+    out_file.write(out)
+except:
+    print('we have egor :(')
+
+out_file.close()
