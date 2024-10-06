650075f2066bd82351b81ab445b4590d7fd230df
ValeriaBelyaeva <lera.belyayeva.2006@mail.ru>
2024-10-06 13:18:59 +0700

    Solve task 2 "sorting by inserts +" and tests

--- lab1/task1/src/insertion_sort_plus.py
+++ lab1/task1/src/insertion_sort_plus.py
@@ -11,21 +11,3 @@ def insertion_sort(to_sort: list)-> (list, list):
         to_sort[j + 1] = key
 
     return swap_id, to_sort
-
-
-test_input = [
-    [5, 4, 3, 2, -1],
-    [31, 41, 59, 26, 41, 58],
-    [1000000000, 1000000000, 50000000],
-    [-1000000000, -1000000000, 50000000],
-    [1, 8, 4, 2, 3, 7, 5, 6, 9, 0],
-    [i for i in range(1000)],
-    [i for i in range(1000, 0, -1)]
-]
-for test in test_input:
-    print(insertion_sort(test))
-
-'''
-1 2 2 2 3 5 5 6 9 1
-0 1 2 3 4 5 6 7 8 9
-'''
