bca908c055055b65a87d91fc53e6eb194e97a8f3
ValeriaBelyaeva <lera.belyayeva.2006@mail.ru>
2024-10-06 14:02:51 +0700

    added work with files

--- lab1/task1/src/insertion_sort_plus.py
+++ lab1/task1/src/insertion_sort_plus.py
@@ -11,3 +11,21 @@ def insertion_sort(to_sort: list)-> (list, list):
         to_sort[j + 1] = key
 
     return swap_id, to_sort
+
+inp_file = open("input")
+inp = list(map(int, inp_file.readline().split()))
+inp_file.close()
+out_file = open("output", 'w')
+try:
+    ans1, ans2 = insertion_sort(inp)
+    out1 = ''
+    for e in ans1:
+        out1 = out1 + str(e) + ' '
+    out2 = ''
+    for e in ans2:
+        out2 = out2 + str(e) + ' '
+    out_file.write(out1+'\n'+out2)
+except:
+    print('we have egor :(')
+
+out_file.close()
